ea920c586ef5eac80408a23aca715e1a31a9ba0f
thiagorocha <thiago.rocha@soulisto.com.br>
2022-07-04 17:00:00 -0300

    first commit quarkus

---
+++ src/test/java/br/com/throchadev/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.throchadev;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
